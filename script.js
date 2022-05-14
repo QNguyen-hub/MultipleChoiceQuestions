f90b81be97637020544b977e343c83298272a32d
Quy Nguyen <qnguyen303@gmail.com>
2022-05-14 15:29:22 -0600

    added timing and next questions function

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ var startBtn = document.querySelector("#start");
 var quizBox = document.querySelector("#container-quiz");
 var quiz = document.querySelector("#quiz");
 var choices = document.querySelector("#choice");
-var button = document.querySelector("button");
 var list = document.querySelector("#answer-list");
-var randomQuestion;
 var curQuestionIndex = 0;
 var answersChoice = document.querySelector("#choice");
+var score = 0;
+var count = 60;
+var timer = document.querySelector("#timer");
 var questions = [
   {
     question: "What is HTML",
@@ -17,30 +18,52 @@ var questions = [
       { text: "HTMC", correct: false },
     ],
   },
+  {
+    question: "What is CSS",
+    answer: [
+      { text: "CSA", correct: false },
+      { text: "CST", correct: false },
+      { text: "CSS", correct: false },
+      { text: "CSR", correct: true },
+    ],
+  },
 ];
 
 startBtn.addEventListener("click", start);
 
 function start() {
+  countDown();
   startBtn.setAttribute("class", "hide");
-  randomQuestions = questions.sort(() => Math.random() - 0.5);
-  quizBox.removeAttribute("class", "hide");
   nextquiz();
 }
 function displayQuestion(questions) {
   quiz.textContent = questions.question;
   questions.answer.forEach(function (answer) {
     var button = document.createElement("button");
-    button.innerText = answer.text;
-    if (answer.correct) {
-      nextquiz;
-    }
-    button.addEventListener("click", selectAnswer);
+    button.textContent = answer.text;
     list.appendChild(button);
+    button.addEventListener("click", function () {
+      if (answer.correct) {
+        list.setAttribute("class", "hide");
+        curQuestionIndex++;
+        score++;
+        nextquiz();
+      } else {
+        countDown - 2;
+      }
+    });
   });
 }
 function nextquiz() {
   displayQuestion(questions[curQuestionIndex]);
 }
 
-function answer() {}
+function countDown() {
+  var countDnTim = setInterval(function () {
+    count--;
+    timer.textContent = "Time: " + count;
+    if (count === 0) {
+      clearInterval(countDnTim);
+    }
+  }, 1000);
+}
