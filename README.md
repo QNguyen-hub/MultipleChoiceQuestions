e8d7a411564464868aca3e06bb22a150d2ab2497
Quy Nguyen <qnguyen303@gmail.com>
2022-05-14 22:48:17 -0600

    adding more questions to javascript

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 This is my challenge to write a mutliple choice questions.
 
-I created questions using Objects
+There were a lot of challenges for me with this assignment.
+
+First road block was trying to get the text to appear from an array that had objects with keys.
+
+Second was getting the questions to move on to the next if it was correct.
+
+I had some code that I reference for idea from classmates and youtube specifically "Web Dev Simplified".
+
+I was able to work and get the code to start going.
+
+The problem I am stuck on is getting the old questions to disappear before the next one appears.
+
+I am submitting this for now and will continue to work on it was I go through this bootcamp and learn new concept.
--- script.js
+++ script.js
@@ -5,26 +5,36 @@ var choices = document.querySelector("#choice");
 var list = document.querySelector("#answer-list");
 var curQuestionIndex = 0;
 var answersChoice = document.querySelector("#choice");
+var scoreTracking = document.querySelector("#score");
 var score = 0;
-var count = 60;
+var count = 90;
 var timer = document.querySelector("#timer");
 var questions = [
   {
     question: "What is HTML",
     answer: [
-      { text: "HTML", correct: true },
-      { text: "HTMA", correct: false },
-      { text: "HTMB", correct: false },
-      { text: "HTMC", correct: false },
+      { text: "Hypertext Markup Language", correct: true },
+      { text: "Hydrotext Maker Launch", correct: false },
+      { text: "Horseteeth Making Log", correct: false },
+      { text: "Hey There Ma La", correct: false },
     ],
   },
   {
     question: "What is CSS",
     answer: [
-      { text: "CSA", correct: false },
-      { text: "CST", correct: false },
-      { text: "CSS", correct: false },
-      { text: "CSR", correct: true },
+      { text: "Case Shop Style", correct: false },
+      { text: "Cash Store Safe", correct: false },
+      { text: "Cascading Sheet Style", correct: false },
+      { text: "Cascading Style Sheets", correct: true },
+    ],
+  },
+  {
+    question: "What is JS",
+    answer: [
+      { text: "Javascript", correct: true },
+      { text: "Java", correct: false },
+      { text: "Javestyle", correct: false },
+      { text: "Javasheets", correct: false },
     ],
   },
 ];
@@ -44,12 +54,12 @@ function displayQuestion(questions) {
     list.appendChild(button);
     button.addEventListener("click", function () {
       if (answer.correct) {
-        list.setAttribute("class", "hide");
+        // list.setAttribute("class", "hide");
         curQuestionIndex++;
         score++;
         nextquiz();
       } else {
-        countDown - 2;
+        count -= 2;
       }
     });
   });
@@ -67,3 +77,4 @@ function countDown() {
     }
   }, 1000);
 }
+scoreTracking.textContent = score;
